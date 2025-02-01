9f1be2b52ccca2108066827eb81bfb8b90d93cd2
sakshikhule <102469697+sakshikhule@users.noreply.github.com>
2025-02-01 21:39:02 +0530

    Create Comp_7.py

---
+++ Comp_7.py
@@ -0,0 +1,3 @@
+#7. Use a nested list comprehension to find all of the numbers from 1–1000 that are divisible by any single digit. 
+num = [num for num in range(1,1001) if any(num % i == 0 for i in range(1,10))]
+print(num)
